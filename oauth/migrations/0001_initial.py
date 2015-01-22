983c5c7a9483da03f57908bbb656cbe9ef44f762
avnash <avi.kondeti@gmail.com>
2015-01-22 10:40:13 +0530

    migrations changed

--- oauth/migrations/0001_initial.py
+++ oauth/migrations/0001_initial.py
@@ -16,6 +16,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('client_id', models.CharField(max_length=40)),
                 ('client_uri', models.CharField(max_length=200)),
+                ('client_name', models.CharField(max_length=40)),
             ],
             options={
             },
@@ -25,8 +26,46 @@ class Migration(migrations.Migration):
             name='ClientCred',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('client_secret', models.CharField(max_length=200)),
+                ('client_secret', models.CharField(max_length=40)),
                 ('client', models.ForeignKey(to='oauth.Client')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Hostel',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('hostel_name', models.CharField(max_length=20)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='LoginRequestLog',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('username', models.TextField()),
+                ('meta_http_host', models.TextField()),
+                ('meta_http_referer', models.TextField()),
+                ('meta_http_user_agent', models.TextField()),
+                ('meta_query_string', models.TextField()),
+                ('meta_remote_addr', models.TextField()),
+                ('meta_remote_host', models.TextField()),
+                ('meta_request_method', models.TextField()),
+                ('timestamp', models.DateTimeField()),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='ResponseType',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('responsetype', models.CharField(default=b'code', max_length=50)),
             ],
             options={
             },
@@ -36,7 +75,7 @@ class Migration(migrations.Migration):
             name='Scope',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('scopename', models.CharField(max_length=50)),
+                ('scope', models.CharField(max_length=50)),
             ],
             options={
             },
@@ -47,6 +86,10 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('username', models.CharField(max_length=100)),
+                ('firstname', models.CharField(max_length=100)),
+                ('lastname', models.CharField(max_length=100)),
+                ('room', models.IntegerField(default=0)),
+                ('hostel', models.ForeignKey(to='oauth.Hostel')),
             ],
             options={
             },
@@ -56,9 +99,12 @@ class Migration(migrations.Migration):
             name='UserAuthLog',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('authorization_code', models.CharField(max_length=40)),
+                ('access_token', models.CharField(max_length=60)),
+                ('trash', models.IntegerField(default=0)),
+                ('expiry', models.DateTimeField()),
                 ('ip', models.CharField(max_length=15)),
                 ('timestamp', models.DateTimeField()),
-                ('client', models.ForeignKey(to='oauth.Client')),
                 ('user', models.ForeignKey(to='oauth.User')),
             ],
             options={
@@ -77,10 +123,21 @@ class Migration(migrations.Migration):
             bases=(models.Model,),
         ),
         migrations.CreateModel(
+            name='UserLog',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('username', models.TextField()),
+                ('count', models.BigIntegerField()),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
             name='Usertype',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('typename', models.CharField(max_length=50)),
+                ('usertype', models.CharField(max_length=50)),
             ],
             options={
             },
@@ -90,6 +147,18 @@ class Migration(migrations.Migration):
             model_name='user',
             name='usertypes',
             field=models.ManyToManyField(to='oauth.Usertype'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='client',
+            name='managers',
+            field=models.ManyToManyField(to='oauth.User'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='client',
+            name='responsetypes',
+            field=models.ManyToManyField(to='oauth.ResponseType'),
             preserve_default=True,
         ),
         migrations.AddField(
